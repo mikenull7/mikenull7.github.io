d2bfba190a3b46e21322cf49a6df5134a8232657
mikenull7 <mikenull7@gmail.com>
2024-12-20 13:36:17 -0500

    consolelog edit

--- socket.js
+++ socket.js
@@ -1,7 +1,7 @@
 export const sosSocket = new WebSocket("ws://localhost:8080");
 
 sosSocket.onopen = () => {
-  console.log("Connected to SOS.");
+  console.log("Connected to SOS. you dumb son of a bitch");
 };
 
 sosSocket.onmessage = (event) => {
