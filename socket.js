141bc17f721e0289766340fb02592a8c947b3cf9
mikenull7 <mikenull7@gmail.com>
2024-12-20 13:50:55 -0500

    alot

--- socket.js
+++ socket.js
@@ -4,6 +4,12 @@ sosSocket.onopen = () => {
   console.log("Connected to SOS. you dumb son of a bitch");
 };
 
+sosSocket.addEventListener("message", (event) => {
+  const message = event.data;
+
+  console.log(message);
+  // Handle incoming messages from the server
+});
 sosSocket.onmessage = (event) => {
   console.log(event.data);
 };
