56f15aff6bd3c94dada949e3e693077e99a375dc
mikenull7 <mikenull7@gmail.com>
2025-07-24 16:28:33 -0400

    reverted back alot commit

--- socket.js
+++ socket.js
@@ -1,7 +1,7 @@
 export const sosSocket = new WebSocket("ws://localhost:49122");
 
 sosSocket.onopen = () => {
-  console.log("Connected to SOS. you dumb son of a bitch");
+  console.log("Connected to SOS. howm any times left");
 };
 
 sosSocket.addEventListener("message", (event) => {
